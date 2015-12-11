aa5c43062563cd8bb08f9c3fc08afa7674c16be2
Chris <ignatz@gmail.com>
2015-12-11 12:34:34 -0500

    Chris - Folded in changes from last Friday's CodeClub

--- clojure/src/mcribadamus/core.clj
+++ clojure/src/mcribadamus/core.clj
@@ -1,5 +1,39 @@
-(ns mcribadamus.core)
+(ns mcribadamus.core
+  (:require [cheshire.core :refer :all]))
 
-(defn get-symbols [month year]
-  "I return a list of symbols relative to the month and year passed in"
-  [1 2 3 4 5 6 7 8 9])
+(defn to-symbol [year symbol-entry]
+  (str (:symbol symbol-entry) year ".CME"))
+
+(defn symbols-after-month [month symbols-map]
+  (drop-while #(> month (:month %)) symbols-map))
+
+(defn symbols-for-year [year month symbols-map]
+  (map #(to-symbol year %)
+        (symbols-after-month month symbols-map)))
+
+(defn future-symbols [start-year start-month symbols-map]
+  (lazy-seq
+    (concat (symbols-for-year start-year start-month symbols-map)
+      (future-symbols (inc start-year) 1 symbols-map))))
+
+(defn symbol-url [symbol]
+  (str "http://finance.yahoo.com/webservice/v1/symbols/"
+    symbol "/quote?format=json&view=detail"))
+
+(defn slurp-symbol [symbol]
+  (slurp (symbol-url symbol)))
+
+(defn decode-entry [key val]
+  (cond
+    (some #(= key %) [:day_low :year_high :volume :ts :year_low :day_high
+                      :chg_percent :change :price])
+        {key (decode val)}
+    :else {key val}))
+
+(defn decode-symbol [data]
+  (->> data (map #(decode-entry (first %) (second %))) (reduce merge) ))
+
+(defn parse-symbol-data [symbol]
+ (let [result (slurp-symbol symbol)]
+  (->> (parse-string result true) :list :resources first :resource :fields
+          decode-symbol)))
--- clojure/test/mcribadamus/core_test.clj
+++ clojure/test/mcribadamus/core_test.clj
@@ -1,10 +1,31 @@
 (ns mcribadamus.core-test
-  (:require [midje.sweet :refer :all]
+  (:use midje.sweet)
+  (:require [clojure.test :refer :all]
             [mcribadamus.core :refer :all]))
 
-(facts "about determining the correct futures symbols"
-  (fact "for a month/year get back at least 9 symbols"
-    (count (get-symbols 0 16)) => 9)
-  (fact "for Jan 2015 the first symbol back should be LHG16.CME"
-    (first (get-symbols 0 16)) => "LHG16.CME")
-       )
+(def symbols [{:symbol "ABC" :month 1} {:symbol "ABD" :month 2}
+  {:symbol "ABE" :month 4} {:symbol "ABF" :month 6}])
+
+(fact "given a symbol and a year it returns a symbol entry"
+    (to-symbol 16 (first symbols)) => "ABC16.CME")
+
+(fact "given a year, month and map of symbols it returns all symbols
+       for the year after the given month"
+    (symbols-for-year 16 2 symbols) => ["ABD16.CME" "ABE16.CME" "ABF16.CME"])
+
+(fact "returns symbols after given year and given month"
+ (take 5 (future-symbols 15 4 symbols)) => ["ABE15.CME" "ABF15.CME" "ABC16.CME"
+                                            "ABD16.CME" "ABE16.CME"])
+
+(fact "future-url takes a symbol and returns a stock URL"
+  (symbol-url "ABC16.CME") => "http://finance.yahoo.com/webservice/v1/symbols/ABC16.CME/quote?format=json&view=detail")
+
+(fact "slurp-symbol returns a string"
+  (first (slurp-symbol "LHG16.CME")) => \{)
+
+(facts "parse-symbol-data "
+  (fact "returns market data for that symbol"
+    (:symbol (parse-symbol-data "LHG16.CME")) =>  "LHG16.CME")
+
+  (fact "returns nil for an invalid symbol"
+    (:symbol (parse-symbol-data "ABC.CME")) => nil))
